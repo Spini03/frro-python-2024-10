a4a66579fcb09e0fb1eb31e9ee1a87f19b607ae3
Spini03 <santiagospini@gmail.com>
2024-04-24 15:53:45 -0300

    formulario con kivy

---
+++ kivy/formulario/formulario.py
@@ -0,0 +1,10 @@
+from kivy.uix.textinput import TextInput
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.relativelayout import RelativeLayout
+
+class formularioApp(App):
+    pass
+
+if __name__ == '__main__':
+    formularioApp().run()
