0833e046c9bf02e6cfcaa48dadf789447e95318b
Spini03 <santiagospini@gmail.com>
2024-06-02 18:40:50 -0300

    popup

--- kivy/ejemplo_popup/popup.py
+++ kivy/ejemplo_popup/popup.py
@@ -4,13 +4,13 @@ from kivy.uix.popup import Popup
 
 class PopupExample(App):
     def show_popup(self):
-        content = Button(text="Cerrar Popup")
-        popup = Popup(title="Ejemplo de Popup", content=content, size_hint=(None, None), size=(200, 200))
+        content = Button(text="Cerrar")
+        popup = Popup(title="Error! Servidor en mantenimiento", content=content, size_hint=(None, None), size=(200, 200))
         content.bind(on_press=popup.dismiss)
         popup.open()
 
     def build(self):
-        button = Button(text="Mostrar Popup", size_hint=(None, None), size=(200, 100), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        button = Button(text="Registrarse", size_hint=(None, None), size=(200, 100), pos_hint={'center_x': 0.5, 'center_y': 0.5})
         button.bind(on_press=lambda instance: self.show_popup())
         return button
 
