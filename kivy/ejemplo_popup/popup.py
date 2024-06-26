21517b460aa8dd1f4b3166ee1b779c30a8a5ea35
Spini03 <santiagospini@gmail.com>
2024-06-26 14:59:53 -0300

    popup terminado

--- kivy/ejemplo_popup/popup.py
+++ kivy/ejemplo_popup/popup.py
@@ -1,18 +1,7 @@
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.popup import Popup
 
-class PopupExample(App):
-    def show_popup(self):
-        content = Button(text="Cerrar Popup")
-        popup = Popup(title="Ejemplo de Popup", content=content, size_hint=(None, None), size=(200, 200))
-        content.bind(on_press=popup.dismiss)
-        popup.open()
-
-    def build(self):
-        button = Button(text="Mostrar Popup", size_hint=(None, None), size=(200, 100), pos_hint={'center_x': 0.5, 'center_y': 0.5})
-        button.bind(on_press=lambda instance: self.show_popup())
-        return button
+class Popup(App):
+    pass
 
 if __name__ == '__main__':
-    PopupExample().run()
+    Popup().run()
